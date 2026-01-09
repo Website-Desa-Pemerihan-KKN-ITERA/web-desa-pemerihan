29b1c83ccac4fbfdec704efbb988e9606ce492b5
sinavarasina <varasinafarmadani@gmail.com>
2026-01-09 20:28:28 +0700

    fix typo at password that suppose to be newPassword

--- app/api/auth/changepassword/route.ts
+++ app/api/auth/changepassword/route.ts
@@ -5,67 +5,67 @@ import { validateBody } from "@/libs/requestHelper";
 
 // zod type validation
 const fromRequest = z.object({
-  username: z.string().min(5),
-  password: z.string().min(5),
-  newPassword: z.string().min(5),
+    username: z.string().min(5),
+    password: z.string().min(5),
+    newPassword: z.string().min(5),
 });
 
 export async function PUT(req: Request) {
-  const result = await validateBody(req, fromRequest);
-  if (!result.success) {
-    return Response.json(
-      { error: result.error },
-      { status: result.error.status },
-    );
-  }
-
-  // query to db to get the user row
-  let user;
-  try {
-    user = await prisma.user.findUnique({
-      where: {
-        name: result.data.username,
-      },
-    });
-
-    // handle password or username is valid or not
-    if (!user || !user.password) {
-      return Response.json(
-        { error: "Invalid username or password" },
-        { status: 401 },
-      );
+    const result = await validateBody(req, fromRequest);
+    if (!result.success) {
+        return Response.json(
+            { error: result.error },
+            { status: result.error.status },
+        );
     }
-  } catch (err) {
-    return Response.json({ error: "Internal Server Error" }, { status: 500 });
-  }
 
-  // compare the password of user request with the hash in the db in the same user
-  const checkPassword = await bcrypt.compare(
-    result.data.password,
-    user.password,
-  ); //bool
-  if (checkPassword === false) {
-    return Response.json({ error: "Password salah" }, { status: 401 });
-  }
-  console.log(user);
+    // query to db to get the user row
+    let user;
+    try {
+        user = await prisma.user.findUnique({
+            where: {
+                name: result.data.username,
+            },
+        });
 
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(result.data.password, salt);
+        // handle password or username is valid or not
+        if (!user || !user.password) {
+            return Response.json(
+                { error: "Invalid username or password" },
+                { status: 401 },
+            );
+        }
+    } catch (err) {
+        return Response.json({ error: "Internal Server Error" }, { status: 500 });
+    }
 
-  try {
-    await prisma.user.update({
-      where: {
-        name: result.data.username,
-      },
-      data: {
-        password: hash,
-      },
+    // compare the password of user request with the hash in the db in the same user
+    const checkPassword = await bcrypt.compare(
+        result.data.password,
+        user.password,
+    ); //bool
+    if (checkPassword === false) {
+        return Response.json({ error: "Password salah" }, { status: 401 });
+    }
+    console.log(user);
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(result.data.newPassword, salt);
+
+    try {
+        await prisma.user.update({
+            where: {
+                name: result.data.username,
+            },
+            data: {
+                password: hash,
+            },
+        });
+    } catch {
+        return Response.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+
+    return Response.json({
+        message: "Password berhasil diubah.",
     });
-  } catch {
-    return Response.json({ error: "Internal Server Error" }, { status: 500 });
-  }
-
-  return Response.json({
-    message: "Password berhasil diubah.",
-  });
 }
